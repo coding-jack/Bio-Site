5797b2ea22ccf9826c5f4a4c589f893feff98963
Jackson Kaufman <jacksonkaufman115@gmail.com>
2017-06-12 20:52:50 -0600

    Added picture fields to bio template and fixed vertical scrolling bug globally

--- wp-content/themes/coding-jack/resources/views/template-bio.blade.php
+++ wp-content/themes/coding-jack/resources/views/template-bio.blade.php
@@ -7,24 +7,24 @@
 @section('content')
   @while(have_posts()) @php(the_post())
     <div class="row hero align-items-center">
-      <div class="container">
+      <div class="container py-5">
         <div class="row align-items-center text-center">
           <div class="col">
             @include('partials.page-header')
             <div class="row py-3">
-              <div class="col-lg-4 offset-lg-1">
+              <div class="col-lg-4 offset-lg-1 pb-3 pb-lg-0">
               {!! get_field('bio') !!}
               </div>
               <div class="col-lg-4 offset-lg-2">
-                <img src="https://media.giphy.com/media/MGYY2FIs8VrfW/giphy.gif" class="w-100 img-fluid">
+                <img src="{{ get_field('picture_1') }}" class="w-100 img-fluid">
               </div>
             </div>
             <div class="row py-3">
-              <div class="col-lg-4 offset-lg-2 push-lg-5">
+              <div class="col-lg-4 offset-lg-2 push-lg-5 pb-3 pb-lg-0">
               {!! get_field('bio_continued') !!}
               </div>
               <div class="col-lg-4 offset-lg-1 pull-lg-6">
-                <img src="https://media.giphy.com/media/3t7RAFhu75Wwg/giphy.gif" class="w-100 img-fluid">
+                <img src="{{ get_field('picture_2')['url'] }}" class="w-100 img-fluid">
               </div>
             </div>
           </div>
--- wp-content/themes/coding-jack/resources/views/template-skills.blade.php
+++ wp-content/themes/coding-jack/resources/views/template-skills.blade.php
@@ -7,13 +7,15 @@
 @section('content')
   @while(have_posts()) @php(the_post())
     <div class="row hero align-items-center">
-      <div class="container">
+      <div class="container py-5">
         <div class="row align-items-center text-center">
           <div class="col">
             <h1 class="text-uppercase mb-5">{{ get_the_title() }}</h1>
             @while(have_rows('skills')) @php(the_row())
               <h3>{{ get_sub_field('label') }}</h3>
-              {!! get_sub_field('details') !!}
+              <div class="pb-2">
+                {!! get_sub_field('details') !!}
+              </div>
             @endwhile
           </div>
         </div>
