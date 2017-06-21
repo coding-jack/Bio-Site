4bfa127c5b76ced77ca9edeb9e1e89175f6ac485
Jackson Kaufman <jacksonkaufman115@gmail.com>
2017-06-20 20:02:02 -0600

    Responsive padding fixes on bio text and adding home icon to header

--- wp-content/themes/coding-jack/resources/views/partials/header.blade.php
+++ wp-content/themes/coding-jack/resources/views/partials/header.blade.php
@@ -1,10 +1,5 @@
-<header class="banner">
-  <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
-    </nav>
-  </div>
-</header>
+@if (!is_front_page())
+  <header class="banner">
+      <a class="brand" href="{{ home_url('/') }}"><i class="fa fa-home fa-2x"></i></a>
+  </header>
+@endif
--- wp-content/themes/coding-jack/resources/views/template-bio.blade.php
+++ wp-content/themes/coding-jack/resources/views/template-bio.blade.php
@@ -12,19 +12,19 @@
           <div class="col">
             @include('partials.page-header')
             <div class="row py-3">
-              <div class="col-lg-4 offset-lg-1 pb-3 pb-lg-0">
+              <div class="col-lg-4 offset-lg-1 pb-3 pb-lg-0 pt-lg-2 pt-xl-3">
               {!! get_field('bio') !!}
               </div>
               <div class="col-lg-4 offset-lg-2">
-                <img src="{{ get_field('picture_1') }}" class="w-100 img-fluid">
+                <img src="{{ get_field('picture_1') }}" class="rounded img-fluid">
               </div>
             </div>
             <div class="row py-3">
-              <div class="col-lg-4 offset-lg-2 push-lg-5 pb-3 pb-lg-0">
+              <div class="col-lg-4 offset-lg-2 push-lg-5 pb-3 pb-lg-0 pt-lg-4 pt-xl-5">
               {!! get_field('bio_continued') !!}
               </div>
               <div class="col-lg-4 offset-lg-1 pull-lg-6">
-                <img src="{{ get_field('picture_2')['url'] }}" class="w-100 img-fluid">
+                <img src="{{ get_field('picture_2')['url'] }}" class="rounded img-fluid">
               </div>
             </div>
           </div>
